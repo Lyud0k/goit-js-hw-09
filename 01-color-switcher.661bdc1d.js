d749619a4555199f8fed78fe5601d93af7f22752
Lyud0k <99692275+Lyud0k@users.noreply.github.com>
2022-08-31 19:10:00 +0000

    Deploying to gh-pages from @ Lyud0k/goit-js-hw-09@eefb02ea5d9cc05fb33fe5aada41687015b16e6a 🚀

---
+++ 01-color-switcher.661bdc1d.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("[data-start]"),o=document.querySelector("[data-stop]");t.addEventListener("click",(function o(){t.disabled=!0,e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,col=setTimeout(o,1e3),console.log(t)})),o.addEventListener("click",(function(){clearTimeout(col),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.661bdc1d.js.map
