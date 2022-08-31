eefb02ea5d9cc05fb33fe5aada41687015b16e6a
Lyud0k <99692275+Lyud0k@users.noreply.github.com>
2022-08-31 22:09:00 +0300

    hw9_2

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,25 @@
+const Box = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener('click', changeColor);
+stopBtn.addEventListener('click', stopChangeColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColor() {
+    startBtn.disabled = true;
+    Box.style.backgroundColor = getRandomHexColor();
+    col = setTimeout(changeColor, 1000);
+    console.log(startBtn);
+}
+
+function stopChangeColor() {
+    clearTimeout(col);
+    startBtn.disabled = false;
+}
+
+
+
---
+++ src/js/02-timer.js
@@ -0,0 +1,95 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const inputCalendar = document.querySelector('#datetime-picker');
+const btn = document.querySelector('[data-start]');
+const valueDays = document.querySelector('[data-days]');
+const valueHours = document.querySelector('[data-hours]');
+const valueMinutes = document.querySelector('[data-minutes]');
+const valueSeconds = document.querySelector('[data-seconds]');
+
+// let selectetData = null;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    const selectetData = new Date(selectedDates[0]);
+    timeCh(selectetData);
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+function timeCh(selectetData) {
+  const tt = selectetData.getTime();
+  if (tt < options.defaultDate) {
+    btn.disabled = true;
+    window.alert("Please choose a date in the future");
+    return;
+  }
+  btn.disabled = false;
+  btn.addEventListener('click', () => {
+btn.disabled = true;
+    const int = setInterval(() => {
+      const dateNow = Date.now();
+      const timeLeft = tt - dateConst;
+      const timeDifference = dateNow - dateConst;
+      const timeCountdown = timeLeft - timeDifference;
+      const { days, hours, minutes, seconds } = convertMs(timeCountdown);
+      console.log(`${days}:${hours}:${minutes}:${seconds}`);
+      valueSeconds.textContent = seconds;
+      valueMinutes.textContent = minutes;
+      valueHours.textContent = hours;
+      valueDays.textContent = days;
+      console.log(`${valueSeconds.textContent}`);
+      console.log(`${valueMinutes.textContent}`);
+      console.log(`${valueHours.textContent}`);
+      console.log(`${valueDays.textContent}`);
+      //     console.log(timeLeft);
+      // console.log(timeDifference);
+      console.log(timeCountdown);
+      // console.log(dateNow);
+      // console.log(dateConst);
+    }, 1000);
+    if (timeCountdown < 0) {
+      clearInterval(int);
+    }
+  });
+
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+const dateConst = Date.now();
+
+  function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  }
+
+
+
